40310658b0a027bdd2b56c015e9370bcc927d05c
rickDo70 <124622686+rickDo70@users.noreply.github.com>
2023-02-25 21:11:26 +0100

    Update flower-card.js

--- flower-card.js
+++ flower-card.js
@@ -268,7 +268,7 @@ customElements.whenDefined("card-tools").then(() => {
                 entity: iu_zone,
                 tap_action: {action: "toggle"},
               });
-            console.log(myElement.innerHtml);
+            //console.log(myElement.innerHtml);
             const iu_zone_attributes = this._hass.states[iu_zone].attributes;
             const iu_enabled = iu_zone_attributes.enabled; // stateAttr...
             const iu_enabled_icon = (iu_zone_attributes.enabled) ? "mdi:toggle-switch-outline" : "mdi:toggle-switch-off-outline";
@@ -307,6 +307,7 @@ customElements.whenDefined("card-tools").then(() => {
             <div class="divider"></div>
             <div class="attributes" style="height: 100px">
                 <span>Test</span>
+                <span>${myElement}</span>
             </div>
             `;
         } else {
